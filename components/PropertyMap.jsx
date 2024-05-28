8b58d6f762040566cdfb76367bdb7bc87df7fddf
Heber Solano <hebersolano1@gmail.com>
2024-05-27 20:54:18 -0600

    property map v0.2

--- components/PropertyMap.jsx
+++ components/PropertyMap.jsx
@@ -1,18 +1,21 @@
 "use client";
 import "mapbox-gl/dist/mapbox-gl.css";
-import Map, { Marker } from "react-map-gl/dist/esm/components/map";
 import { setDefaults, fromAddress } from "react-geocode";
 import { useEffect, useState } from "react";
+import MiniSpinner from "./MiniSpinner";
+import mapboxgl from "mapbox-gl";
+import Map from "react-map-gl";
 
 function PropertyMap({ property }) {
   const [lat, setLat] = useState(null);
   const [lng, setLng] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
   const [viewport, setViewport] = useState({
     latitude: 0,
     longitude: 0,
     zoom: 12,
     width: "100%",
-    height: "500px",
+    height: 500,
   });
 
   setDefaults({
@@ -26,16 +29,48 @@ function PropertyMap({ property }) {
   useEffect(
     function () {
       async function fetchCoords() {
-        const res = await fromAddress(`${street} ${city} ${state} ${zipcode}`);
-        const { lat, lng } = res.results[0].geometry.location;
-        console.log(lat, lng);
+        try {
+          setIsLoading(true);
+          const res = await fromAddress(
+            `${property.location.street} ${property.location.city} ${property.location.state} ${property.location.zipcode}`
+          );
+          const { lat, lng } = res.results[0].geometry.location;
+          setLat(lat);
+          setLng(lng);
+          setViewport({ ...viewport, latitude: lat, longitude: lng });
+          setIsLoading(false);
+        } catch (error) {
+          console.error("fetchCoords error:", error);
+        }
       }
-      fetchCoords();
+      fetchCoords().catch((e) => console.log(e));
     },
-    [street, city, state, zipcode]
+    [viewport, property.location]
   );
 
-  return <div>MapBox Map</div>;
+  if (isLoading)
+    return (
+      <div className="flex justify-center">
+        <MiniSpinner />
+      </div>
+    );
+
+  console.log("data: ", lat, lng, viewport);
+
+  if (!isLoading)
+    return (
+      <Map
+      // mapboxAccessToken={process.env.NEXT_PUBLIC_MAPBOX_TOKEN}
+      // mapLib={mapboxgl}
+      // initialViewState={{
+      //   latitude: lng,
+      //   longitude: lat,
+      //   zoom: 15,
+      // }}
+      // style={{ width: "100%", height: 500 }}
+      // mapStyle="mapbox://styles/mapbox/streets-v12"
+      />
+    );
 }
 
 export default PropertyMap;
